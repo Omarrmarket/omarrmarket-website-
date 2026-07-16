5271f92322c88c4de28dcf77e5d3b168d116db03
Omarrmarket <oyenekanprince1@gmail.com>
2026-07-17 00:08:54 +0100

    Implement cart functionality and product search

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// Marketplace JavaScript
 
+let cart = [];
+
+function addToCart(name, price) {
+  cart.push({
+    name: name,
+    price: price
+  });
+
+  alert(name + " has been added to your cart!");
+
+  console.log(cart);
+}
+
+function searchProducts() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let products = document.querySelectorAll(".product");
+
+  products.forEach(product => {
+    let title = product.querySelector("h3").textContent.toLowerCase();
+
+    if (title.includes(input)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+}
